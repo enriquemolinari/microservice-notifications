52727bbf940e38e3b028ddebdb2e98ae0c7d6abe
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2020-07-23 20:27:14 -0300

    redis server and port now is configurable by a jvm arg

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Objects;
+
 import notifications.infrastructure.EmailVendorProvider;
 import notifications.infrastructure.RedisSubscriber;
 import notifications.model.EmailDispatcher;
@@ -7,7 +9,14 @@ import notifications.model.EmailDispatcher;
 public class Main {
 
  public static void main(String[] args) {
-  var mail = new EmailDispatcher(new RedisSubscriber("localhost", 6379),
+  String pubsub = Objects
+    .requireNonNullElse(System.getProperty("pub-sub-server"), "localhost");
+
+  String pubsubPort = Objects
+    .requireNonNullElse(System.getProperty("pub-sub-port"), "6379");
+
+  var mail = new EmailDispatcher(
+    new RedisSubscriber(pubsub, Integer.valueOf(pubsubPort)),
     new EmailVendorProvider());
   mail.startListening();
  }
